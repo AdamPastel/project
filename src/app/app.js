fb8b1366ecb2ff5a9fd388d1d7ef3403fcb14745
AntoineVff <antoinevuillaume9@gmail.com>
2025-10-16 11:10:29 +0200

    Ajout de la documentation

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,10 @@
 
-
+/**
+ * Méthode effectuant la somme de 2 valeurs
+ * @param {Number} val1 
+ * @param {Number} val2 
+ * @returns La somme de val1 et val2
+ */
 function addition(val1, val2) {
-    return val1 + val2
+    return val1 + val2;
 }
